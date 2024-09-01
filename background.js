27c5fc300d7eab32755620268ffb31591e2ba7e9
cmderror <cmderror@icloud.com>
2024-09-01 04:46:21 +0000

    update

--- background.js
+++ background.js
@@ -2,4 +2,9 @@ browser.newtab.override({
     url: "newtab.html"
 })
 
+
+document.addEventListener('DomContentLoader', ()=>{
+    console.log("nag:",nagivator,"and", 'getBattery' in nagivator)
+})
+
 console.log(1)
