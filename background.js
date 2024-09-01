f23a97ab60ea8c8e9edd8509c1cdf8675ba3af37
cmderror <cmderror@icloud.com>
2024-09-01 15:55:59 +0000

    update

--- background.js
+++ background.js
@@ -1,33 +1,31 @@
 
-browser.runtime.onMessage.addListener(async (msg, sender, sendResponse)=>{
+browser.runtime.onMessage.addListener((msg, sender, sendResponse)=>{
     if(msg["command"] == "getBatteryStatus"){
-        res = await getBatteryStatus()
-        console.log(res)
-        if (res) {
+        port = getBatteryStatus()
+        port.onMessage.addListener((res) => {
+            port.disconnect();
+            console.log(res)
+            if (!res.success)
+                throw new Error("Error accourated on app type")
+            
             return new Promise((resolve)=>{
                 resolve({ data: res, success: true })
             })
-        }
+            
+        });
     }
 });
 
-async function getBatteryStatus() {
+function getBatteryStatus() {
     const port = browser.runtime.connectNative("com.battery");
     port.postMessage({ command: "getBatteryStatus" });
-
-    await port.onMessage.addListener((res) => {
-        port.disconnect();
-        
-        if (!res.success)
-            throw new Error("Error accourated on app type")
-        
-        return res
-    });
-
+    
     port.onDisconnect.addListener((p) => {
         if (browser.runtime.lastError)            
             throw new Error(`Disconnected due to error: ${browser.runtime.lastError.message}`);
 
         console.log("Port disconnected");
     });
+    
+    return port
 }
