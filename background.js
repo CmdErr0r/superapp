3ffe8b31713cd37bf28444e1a94d8ca5a6eb1ed3
cmderror <cmderror@icloud.com>
2024-09-01 15:53:26 +0000

    update

--- background.js
+++ background.js
@@ -11,25 +11,17 @@ browser.runtime.onMessage.addListener(async (msg, sender, sendResponse)=>{
     }
 });
 
-function getBatteryStatus() {
+async function getBatteryStatus() {
     const port = browser.runtime.connectNative("com.battery");
     port.postMessage({ command: "getBatteryStatus" });
 
-    port.onMessage.addListener((res) => {
+    await port.onMessage.addListener((res) => {
         port.disconnect();
         
-        console.log("res")
-        console.log(res)
-
         if (!res.success)
             throw new Error("Error accourated on app type")
         
         return res
-        
-        // browser.tabs.query({ active:true, currentWindow:true }).then((tabs)=>{
-        //     browser.tabs.sendMessage(tabs[0].id, { command:"BatteryStatus", data:res })
-        // })
-
     });
 
     port.onDisconnect.addListener((p) => {
